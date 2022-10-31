0003c0896e707ba5988269dfe97e1ce94f6c4afd
Ritvik-2002 <2002ritvik@gmail.com>
2022-10-31 13:11:57 +0530

    Modifed files

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -10,11 +10,27 @@ import Newsletter from './Newsletter';
 import Feedback from './Feedback';
 import Sliding from './Slinding';
 import Footer from './Footer';
+import Server from './Server';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
-        <><Head />
-            <Interactive /><Translate /><Style /><Founders /><Portfolio /><Contactus /><Newsletter /><Feedback /><Sliding /><Footer /></>
+        <>
+            <BrowserRouter><Routes>
+                <Route path='/' element={<Server />} />
+                <Route path='/head' element={<Head />} />
+                <Route path='/interactive' element={<Interactive />} />
+                <Route path='/translate' element={<Translate />} />
+                <Route path='/style' element={<Style/>} />
+                <Route path='/founders' element={<Founders />} />
+                <Route path='/portfolio' element={<Portfolio />} />
+                <Route path='/contactus' element={<Contactus />} />
+                <Route path='/newsletter' element={<Newsletter />} />
+                <Route path='/feedback' element={<Feedback />} />
+                <Route path='/sliding' element={<Sliding />} />
+                <Route path='/footer' element={<Footer />} />
+            </Routes>
+            </BrowserRouter></>
     );
 }
 export default App;
